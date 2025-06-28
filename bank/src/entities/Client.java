d0c537fc3d7551c08461062a85c08dc9e21cf87a
GabrielNat1 <dcbiel846@gmail.com>
2025-06-28 13:58:32 -0300

    first commit

---
+++ bank/src/entities/Client.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Client {
+    private String name;
+    private int cpf;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
