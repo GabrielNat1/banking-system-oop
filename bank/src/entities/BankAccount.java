10db34b4d183f212a136d70bd889117405fb97db
GabrielNat1 <dcbiel846@gmail.com>
2025-06-28 15:30:34 -0300

    feat: create methods in entities bankaccount

--- bank/src/entities/BankAccount.java
+++ bank/src/entities/BankAccount.java
@@ -22,4 +22,18 @@ public class BankAccount {
     public Client getClient() {
         return client;
     }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
 }
