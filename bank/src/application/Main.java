2d15d171ce340ee4ba136b41c1925102e766e94d
GabrielNat1 <dcbiel846@gmail.com>
2025-06-28 14:38:20 -0300

    feat: added Bank service and initial configuration application Main

--- bank/src/application/Main.java
+++ bank/src/application/Main.java
@@ -1,8 +1,27 @@
 package application;
 
+import service.Bank;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-------------");
+        Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank();
+        int option;
+
+        do {
+            System.out.println("\n1. Criar conta");
+            System.out.println("2. Depositar");
+            System.out.println("3. Sacar");
+            System.out.println("4. Transferir");
+            System.out.println("5. Listar contas");
+            System.out.println("0. Sair");
+            System.out.print("Escolha: ");
+            option = scanner.nextInt();
+
+        } while (option != 0);
+
+        scanner.close();
     }
 }
--- bank/src/service/Bank.java
+++ bank/src/service/Bank.java
@@ -1,4 +1,22 @@
 package service;
 
+import entities.BankAccount;
+
 public class Bank {
-}
+    private BankAccount[] accounts = new BankAccount[100];
+    private int count = 0;
+
+    public void AddAccount(BankAccount account){
+        if (count < accounts.length){
+            accounts[count++] = account;
+        }
+    }
+
+    public BankAccount findAccount(int number) {
+        for (int i = 0; i < count; i++) {
+            if (accounts[i].getNumber() == number) {
+                return accounts[i];
+            }
+        }
+    }
+    }
